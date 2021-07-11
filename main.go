b855a5686267227dc93867789551ef1e9faaa355
moloch-- <875022+moloch--@users.noreply.github.com>
2021-07-11 07:08:56 -0500

    Added bruteforce option

--- main.go
+++ main.go
@@ -3,25 +3,19 @@ package main
 import (
 	"fmt"
 	"math/big"
-	"os"
 	"time"
 )
 
 func main() {
-	startValue := new(big.Int)
-	var raw string
-	fmt.Printf("Start: ")
-	fmt.Scanf("%s", &raw)
-	startValue.SetString(raw, 10)
-
-	if startValue.Cmp(big.NewInt(0)) != 1 {
-		fmt.Printf("Starting value must be greater than 0\n")
-		os.Exit(1)
+	count := big.NewInt(1)
+	for {
+		count.Add(count, big.NewInt(1))
+		startTime := time.Now()
+		value := big.NewInt(0)
+		value.Set(count)
+		steps := collatz(value)
+		fmt.Printf("\r%d Reached 1 in %d step(s) in %s       ", count, steps, time.Since(startTime))
 	}
-
-	startTime := time.Now()
-	steps := collatz(startValue)
-	fmt.Printf("Reached 1 in %d step(s) in %s\n", steps, time.Now().Sub(startTime))
 }
 
 func collatz(start *big.Int) uint64 {
@@ -29,18 +23,17 @@ func collatz(start *big.Int) uint64 {
 	two := big.NewInt(2)
 	three := big.NewInt(3)
 
-	value := start
+	value := start.Add(start, big.NewInt(0))
 	steps := uint64(0)
 	for value.Cmp(one) != 0 {
 		if value.Bit(0) == 0 {
-			value = value.Div(value, two)
+			value.Div(value, two)
 		} else {
 			// 3n+1 is always even
-			value = value.Mul(value, three)
-			value = value.Add(value, one)
-			value = value.Div(value, two)
+			value.Mul(value, three)
+			value.Add(value, one)
+			value.Div(value, two)
 		}
-		fmt.Printf("%d\n", value)
 		steps++
 	}
 	return steps
